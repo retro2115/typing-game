c9d9c20e80003c550899aa9647dbc6041d074be3
Mirza Cerim <retro2115@outlook.com>
2023-10-08 23:16:45 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,12 @@ import { faker } from '@faker-js/faker';
 })
 export class AppComponent {
   title = 'typing-game';
+  enteredText = '';
 
   randomText = faker.lorem.words(5);
   onInput(value: string) {
 	console.log(value)
+	this.enteredText = value;
   }
 
 }
